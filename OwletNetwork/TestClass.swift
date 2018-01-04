f96c960ecbbfef036be827186c35a159b012ed4c
Lorraine Alexander <lorraine.hilton91@gmail.com>
2018-01-04 11:05:41 -0700

    pod spec

---
+++ OwletNetwork/TestClass.swift
@@ -0,0 +1,14 @@
+//
+//  TestClass.swift
+//  OwletNetwork
+//
+//  Created by Lorraine Alexander on 1/4/18.
+//  Copyright © 2018 Owlet Baby Care. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class TestClass: DatabaseReference {
+    
+}
