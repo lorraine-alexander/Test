abfef254963bfe19aed7278ac248b7cc5559cb0c
Lorraine Alexander <lorraine.hilton91@gmail.com>
2018-01-04 10:59:55 -0700

    Initial Commit

---
+++ OwletNetwork/OwletNetwork.h
@@ -0,0 +1,19 @@
+//
+//  OwletNetwork.h
+//  OwletNetwork
+//
+//  Created by Lorraine Alexander on 1/4/18.
+//  Copyright © 2018 Owlet Baby Care. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OwletNetwork.
+FOUNDATION_EXPORT double OwletNetworkVersionNumber;
+
+//! Project version string for OwletNetwork.
+FOUNDATION_EXPORT const unsigned char OwletNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OwletNetwork/PublicHeader.h>
+
+
